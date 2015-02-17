19cdc4e3383503999260b6975326be5e850f80b6
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-02-17 13:50:22 +0100

    Typo.

--- classes/property.php
+++ classes/property.php
@@ -6,13 +6,11 @@ final class property extends label
 {
 	function __construct($value)
 	{
-		$domainException = null;
-
 		try
 		{
 			parent::__construct($value);
 		}
-		catch (\exception $domainException)
+		catch (\exception $exception)
 		{
 			throw new exception\domain('Property should be a string matching [a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*');
 		}
